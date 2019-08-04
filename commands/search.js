994c526015258eb681b84d1b68e9182ef61956e7
26841 <52977656+26841@users.noreply.github.com>
2019-08-03 23:37:40 -0700

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -103,6 +103,7 @@ module.exports = {
 		}
 
 		function embed(matchRow) {
+			console.log(matchRow);
 			message.channel.send({ embed: {
 				color: 0x0099ff,
 				title: `${matchRow[0]}` + ': ' + `${matchRow[1]}`,
