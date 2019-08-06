90cdc2f02eec06b3a85ac28b59b022ef3232877f
26841 <52977656+26841@users.noreply.github.com>
2019-08-05 18:45:00 -0700

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -104,6 +104,8 @@ module.exports = {
 
 		function embed(matchRow) {
 			const date = new Date();
+			console.log(date.getUTCHours() + 55);
+			console.log(date.getHours() + 55);
 			message.channel.send({ embed: {
 				color: 0x0099ff,
 				title: `${matchRow[0]}` + ': ' + `${matchRow[1]}`,
