e0308597b94256622168064dc6ac60b4d191176d
26841 <52977656+26841@users.noreply.github.com>
2019-08-18 14:58:49 -0700

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -3,34 +3,74 @@ module.exports = {
 	description: 'Lists all the searchable information groups',
 	execute(message) {
 		message.channel.send({
-			color: 0x0099ff,
-			title: 'Some title',
-			description: 'Some description here',
-			fields: [
-				{
-					name: 'Regular field title',
-					value: 'Some value here',
-				},
-				{
-					name: '\u200b',
-					value: '\u200b',
-				},
-				{
-					name: 'Inline field title',
-					value: 'Some value here',
-					inline: true,
-				},
-				{
-					name: 'Inline field title',
-					value: 'Some value here',
-					inline: true,
-				},
-				{
-					name: 'Inline field title',
-					value: 'Some value here',
-					inline: true,
-				},
-			],
+			embed: {
+				color: 0x0099ff,
+				title: 'Some title',
+				description: 'Some description here',
+				fields: [
+					{
+						name: 'Regular field title',
+						value: 'Some value here',
+					},
+					{
+						name: '\u200b',
+						value: '\u200b',
+					},
+					{
+						name: 'Inline field title',
+						value: 'Some value here',
+						inline: true,
+					},
+					{
+						name: 'Inline field title',
+						value: 'Some value here',
+						inline: true,
+					},
+					{
+						name: 'Inline field title',
+						value: 'Some value here',
+						inline: true,
+					},
+				],
+			},
+		});
+	},
+};
+module.exports = {
+	name: 'search',
+	description: 'Lists all the searchable information groups',
+	execute(message) {
+		message.channel.send({
+			embed: {
+				color: 0x0099ff,
+				title: 'Some title',
+				description: 'Some description here',
+				fields: [
+					{
+						name: 'Regular field title',
+						value: 'Some value here',
+					},
+					{
+						name: '\u200b',
+						value: '\u200b',
+					},
+					{
+						name: 'Inline field title',
+						value: 'Some value here',
+						inline: true,
+					},
+					{
+						name: 'Inline field title',
+						value: 'Some value here',
+						inline: true,
+					},
+					{
+						name: 'Inline field title',
+						value: 'Some value here',
+						inline: true,
+					},
+				],
+			},
 		});
 	},
 };
