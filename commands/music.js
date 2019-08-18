b664472e21b20523f9120f9a12a76954fc65dcd5
26841 <52977656+26841@users.noreply.github.com>
2019-08-18 15:48:51 -0700

    Update music.js

--- commands/music.js
+++ commands/music.js
@@ -4,13 +4,14 @@ const client = new Discord.Client();
 module.exports = {
 	name: 'music',
 	description: 'Play music',
-	execute(message) {
+	async execute(message) {
 		if (!message.guild) return;
-  		// Only try to join the sender's voice channel if they are in one themselves
-  		if (message.member.voice.channel) {
+		// Only try to join the sender's voice channel if they are in one themselves
+		if (message.member.voice.channel) {
 			const connection = await message.member.voice.channel.join();
-  		} else {
+		}
+		else {
 			message.reply('You need to join a voice channel first!');
-  		}
-	}
+		}
+	},
 };
