8c151c169cee8698b8fb0561899553f63bab6114
26841 <52977656+26841@users.noreply.github.com>
2019-08-17 22:54:51 -0700

    Create art.js

---
+++ commands/art.js
@@ -0,0 +1,30 @@
+const Booru = require('booru');
+const { BooruError, sites } = require('booru');
+// for ES6:
+// import Booru, { search, BooruError, sites } from 'booru'
+const site = 'safebooru';
+
+module.exports = {
+	name: 'art',
+	description: 'Get a random image from safebooru',
+	execute(message) {
+		Booru.search(site, message, { limit: 1, random: false })
+			.then(posts => {
+				// Log the direct link to each image
+				for (const post of posts) {
+					console.log(post.fileUrl);
+				}
+			})
+			.catch(err => {
+				if (err instanceof BooruError) {
+					// It's a custom error thrown by the package
+					// Typically results from errors the boorus returns, eg. "too many tags"
+					console.error(err.message);
+				}
+				else {
+					// This means something pretty bad happened
+					console.error(err);
+				}
+			});
+	},
+};
