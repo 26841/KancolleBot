9b3a66f288334c2a765a3a643fe6d6f35983be5d
26841 <52977656+26841@users.noreply.github.com>
2019-08-19 11:13:51 -0700

    Create nsfw.js

---
+++ commands/nsfw.js
@@ -0,0 +1,38 @@
+const Booru = require('booru');
+const { BooruError, sites } = require('booru');
+// for ES6:
+// import Booru, { search, BooruError, sites } from 'booru'
+const site = 'xbooru';
+
+module.exports = {
+	name: 'nsfw',
+	description: 'Get a random nsfw image from xbooru (only functional in nsfw channels)',
+	execute(message, args) {
+		Booru.search(site, args, { limit: 2, random: true })
+			.then(posts => {
+				// Log the direct link to each image
+				console.log(posts);
+				for (const post of posts) {
+					console.log(post);
+					console.log(post.fileUrl);
+					if (post) {
+						message.channel.send(post.fileUrl);
+					}
+					else {
+						message.channel.send('Chickens!');
+					}
+				}
+			})
+			.catch(err => {
+				if (err instanceof BooruError) {
+					// It's a custom error thrown by the package
+					// Typically results from errors the boorus returns, eg. "too many tags"
+					console.error(err.message);
+				}
+				else {
+					// This means something pretty bad happened
+					console.error(err);
+				}
+			});
+	},
+};
