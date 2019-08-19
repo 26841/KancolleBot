2593e61e581df877d75f579898a66bc8f954b1ad
26841 <52977656+26841@users.noreply.github.com>
2019-08-19 11:10:53 -0700

    Update nsfw-test.js

--- commands/nsfw-test.js
+++ commands/nsfw-test.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'nsfw-test',
 	description: 'Test for checking nsfw',
 	execute(message) {
+		console.log('NSFW test');
 		if (message.channel.nsfw) {
 			message.channel.send('Test');
 		}
