351bf956e2fc7ab66b044f1408e8f539467f8b2d
26841 <michaelwen9@gmail.com>
2019-10-07 22:04:55 -0700

    .

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,18 @@
+const patt = /(^|[\s]+)[pP]+[oO]+[iI]+([-~!?.*_\s]+|$)/i;
+const poi = ['Poi!', '!ioP', 'POI!', 'Pooooiiiii!', 'POOOOIIIII!', 'ぽい!', 'ぽーい!'];
+
 module.exports = {
 	name: 'say',
 	description: 'Ask Poi to say something!',
 	cooldown: 5,
 	execute(message, args) {
-    let text = args.join(" ");
-    message.delete();
-    message.channel.send(text);
+		const text = args.join(' ');
+		message.delete();
+		if (String(text).match(patt)) {
+			message.channel.send(poi[Math.floor(Math.random() * poi.length)]);
+		}
+		else {
+			message.channel.send(text);
+		}
 	},
 };
