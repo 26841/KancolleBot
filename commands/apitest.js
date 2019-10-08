4dc9e3faf8ec467caa2810958c6283e502b9c6a9
JASPER CHUA JUN KIAT <17019686@rp.edu.sg>
2019-10-08 13:32:13 +0800

    test

--- commands/apitest.js
+++ commands/apitest.js
@@ -4,11 +4,8 @@ module.exports = {
 	name: 'apitest',
 	description: 'Test Kancolle api',
 	execute(message) {
-<<<<<<< HEAD
-		let test = wiki.ship['Mutsuki']
+		let test = wiki.ship['Mutsuki'];
 		message.channel.send(test);
-=======
 		console.log(`${wiki.ship['mutsuki']}`);
->>>>>>> 53a26a4b0b7089199c2dd56f78cdd0c0ccdd2ee2
 	},
 };
