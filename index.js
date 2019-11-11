4b43c7643efc46ad3254c3ac59b7a8602db17135
26841 <52977656+26841@users.noreply.github.com>
2019-11-11 03:27:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -96,14 +96,14 @@ client.on('message', message => {
 });
 
 function scheduledMessageTest() {
-	const job = new cron('0 */10 * * * *', function() {
+	const job = new cron('0 0 */1 * * *', function() {
 		const d = new Date();
 		client.guilds.forEach(g =>
 			g.channels
 				.filter(c => c.type === 'text' && c.permissionsFor(g.me).has('SEND_MESSAGES'))
 				.sort((a, b) => b.position - a.position)
 				.first()
-				.send('Every Tenth Minute:', d)
+				.send('Every Hour', d)
 				.catch(e => console.error(`Could not send to ${g.name}:`, e)),
 		);
 	});
