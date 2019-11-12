0ab84fed50842e6d75fd60b1f0f1ffb41a93ebf8
XhacKX <30612883+jasperchua99@users.noreply.github.com>
2019-11-12 17:01:11 +0800

    Update bio-npm.js
    test

--- commands/bio-npm.js
+++ commands/bio-npm.js
@@ -5,9 +5,9 @@ module.exports = {
 	description: 'Reworked bio command using @kancolle/data npm',
 	cooldown: 5,
 	execute(message, args) {
-		const name = args.join(' ')
+		const name = message.content.split(/\s+/g).slice(1).join(" ");
 		console.log(name);
-		const test = wiki.ship[name];
+		const test = wiki.ship[`${name}`];
 		console.log(test);
 		message.channel.send(`${test._japanese_name}`);
 	},
