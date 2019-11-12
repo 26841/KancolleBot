7a855969748711708aac4db51bc0ac45587cfeef
26841 <michael26841@gmail.com>
2019-11-12 01:23:39 -0800

    -

--- commands/arttest.js
+++ commands/arttest.js
@@ -1,31 +1,25 @@
-const Booru = require('booru');
-const { BooruError, sites } = require('booru');
+const { each } = require('bluebird');
+const _ = require('lodash');
+const { search } = require('booru');
+
+const site = process.env.booru;
 // for ES6:
 // import Booru, { search, BooruError, sites } from 'booru'
-const site = 'danbooru';
 
 module.exports = {
-	name: 'arttest',
-	description: 'Get a random image from danbooru, test using filter',
-	execute(message, args) {
-		Booru.search(site, args, { limit: 10, random: true })
-			.then(posts => {
-				console.log(posts);
-				// Log the direct link to each image
-				posts.filter(post => post.rating === 's')
-					.slice(0, 2)
-					.forEach(post => message.channel.send(post.postView));
-			})
-			.catch(err => {
-				if (err instanceof BooruError) {
-					// It's a custom error thrown by the package
-					// Typically results from errors the boorus returns, eg. "too many tags"
-					console.error(err.message);
-				}
-				else {
-					// This means something pretty bad happened
-					console.error(err);
-				}
-			});
+	name: 'art',
+	description: 'Get two random images from danbooru',
+	async execute(message, args) {
+		try {
+			await each(
+				_(await search(site, args, { limit: 100, random: true }))
+					.filter(post => (post || {}).rating === 's')
+					.take(2),
+				post => message.channel.send(post.postView),
+			);
+		}
+		catch (error) {
+			console.error(error.message || error);
+		}
 	},
 };
