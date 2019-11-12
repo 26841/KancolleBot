ade4b7dcb9b0a4dd21a1a82dfbbde4bd58888b66
26841 <michael26841@gmail.com>
2019-11-12 00:43:10 -0800

    -

--- commands/bio-npm.js
+++ commands/bio-npm.js
@@ -5,8 +5,8 @@ module.exports = {
 	description: 'Reworked bio command using @kancolle/data npm',
 	cooldown: 5,
 	execute(message, args) {
-		console.log(args);
-		const test = ships.ship[args];
+		console.log(args.join(' '));
+		const test = ships.ship[args.join(' ')];
 		console.log(test);
 		message.channel.send(test._japanese_name);
 	},
