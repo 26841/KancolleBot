fcca50d0624b883aff88150fb21e3b057a63693c
26841 <michael26841@gmail.com>
2019-11-12 17:42:20 -0800

    -

--- commands/birthdaytest.js
+++ commands/birthdaytest.js
@@ -4,8 +4,11 @@ module.exports = {
 	name: 'birthdaytest',
 	description: 'Json read test',
 	cooldown: 5,
-	execute(message) {
-		const obj = birthdays._1._6;
+	execute(message, args) {
+		if (args.length != 2) {
+			message.reply('Please provide a month and a day in numbers!');
+		}
+		const obj = birthdays.args[0].args[1];
 		console.log(obj);
 		for (const key in obj) {
 			message.channel.send(' name = ' + key + ' year = ' + obj[key]);
