7dc093c863369f03ef6bf93ede4d46d57d5a4656
26841 <michael26841@gmail.com>
2019-11-13 02:11:38 -0800

    -

--- commands/translatejp.js
+++ commands/translatejp.js
@@ -1,4 +1,4 @@
-const translate = require('yandex-translate')(trnsl.1.1.20191113T094553Z.4e217d4c8f27d2a9.3b2dc75be88ece54ef9f7e709859a443c4886239);
+const translate = require('yandex-translate')(TRANSLATE_KEY);
 
 module.exports = {
 	name: 'translatejp',
