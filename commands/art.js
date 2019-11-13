fd42cf8130114b79b204d87e9b6d660245a9f3eb
26841 <52977656+26841@users.noreply.github.com>
2019-11-13 14:49:41 -0800

    Update art.js

--- commands/art.js
+++ commands/art.js
@@ -20,6 +20,7 @@ module.exports = {
 			);
 		}
 		catch (error) {
+			console.log(args.length);
 			if (args.length > 2) {
 				message.reply('You cannot search for more than two tags at a time!');
 			}
