08d2bf0cbfba9083106771232d566ea2c9636879
26841 <michael26841@gmail.com>
2019-11-13 11:27:09 -0800

    birthday function renames

---
+++ commands/birthday(obs).js
@@ -0,0 +1,40 @@
+const birthdays = require('../birthday.json');
+const Discord = require('discord.js');
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+module.exports = {
+	name: 'birthday(obs)',
+	description: 'Birthday search function (obs)',
+	cooldown: 5,
+	execute(message, args) {
+		const d = new Date();
+		let month;
+		let day;
+		if (!args.length) {
+			month = d.getMonth() + 1;
+			day = d.getDate();
+		}
+		else if (args.length == 2) {
+			month = args[0];
+			day = args[1];
+		}
+		else {
+			return message.reply('You need to provide a month and a day!');
+		}
+		const obj = birthdays['_' + month]['_' + day];
+		if (obj) {
+			const richembed = new Discord.RichEmbed()
+				.setTitle('Here\'s everyone with a birthday on ' + months[month - 1] + ' ' + day);
+			for (const key in obj) {
+				const title = key + ' - ' + obj[key];
+				const birthday = new Date(obj[key], month, day);
+				const age_dt = new Date(Date.now() - birthday.getTime());
+				const age = Math.abs(age_dt.getUTCFullYear() - 1970);
+				const snippet = 'Currently ' + age + ' years old';
+				richembed.addField(title, snippet);
+			}
+			message.channel.send(richembed);
+		}
+		else {message.channel.send('No one has a birthday on ' + months[month - 1] + ' ' + day + '!');}
+	},
+};
--- commands/birthday.js
+++ commands/birthday.js
@@ -1,40 +1,45 @@
-const birthdays = require('../birthday.json');
 const Discord = require('discord.js');
+const d = new Date();
+const birthdays = require('../birthday.json');
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function ordinal(date) {
+	return (date > 20 || date < 10) ? ([false, 'st', 'nd', 'rd'])[(date % 10)] || 'th' : 'th';
+}
 
 module.exports = {
 	name: 'birthday',
-	description: 'Json read test',
+	description: 'Birthday search function',
 	cooldown: 5,
 	execute(message, args) {
-		const d = new Date();
 		let month;
 		let day;
-		if (!args.length) {
-			month = d.getMonth() + 1;
-			day = d.getDate();
-		}
-		else if (args.length == 2) {
+
+		if (args.length == 2) {
 			month = args[0];
 			day = args[1];
 		}
+		else if (!args.length) {
+			month = d.getMonth() + 1;
+			day = d.getDate();
+		}
 		else {
-			return message.reply('You need to provide a month and a day!');
+			return message.reply('Invalid Birthday, Do provide one in this format(mm dd)');
 		}
-		const obj = birthdays['_' + month]['_' + day];
-		if (obj) {
-			const richembed = new Discord.RichEmbed()
-				.setTitle('Here\'s everyone with a birthday on ' + months[month - 1] + ' ' + day);
-			for (const key in obj) {
-				const title = key + ' - ' + obj[key];
-				const birthday = new Date(obj[key], month, day);
-				const age_dt = new Date(Date.now() - birthday.getTime());
-				const age = Math.abs(age_dt.getUTCFullYear() - 1970);
-				const snippet = 'Currently ' + age + ' years old';
-				richembed.addField(title, snippet);
-			}
-			message.channel.send(richembed);
+
+		const obj = birthdays[`_${month}`][`_${day}`];
+		if (obj === undefined) {
+			return message.channel.send('No one has a birthday on ' + months[month - 1] + ' ' + day + '!');
 		}
-		else {message.channel.send('No one has a birthday on ' + months[month - 1] + ' ' + day + '!');}
+
+		const richembed = new Discord.RichEmbed()
+			.setTitle(`Here's everyone with a birthday on ${months[month - 1]} ${day}${ordinal(day)}`);
+		for (const key in obj) {
+			const birthday = new Date(obj[key], month, day);
+			const age_dt = new Date(Date.now() - birthday.getTime());
+			const age = Math.abs(age_dt.getUTCFullYear() - 1970);
+			richembed.addField(`${key} - ${obj[key]}`, `${age} years old`, true);
+		}
+		message.channel.send(richembed);
 	},
 };
