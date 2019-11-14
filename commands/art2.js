bdc80b816fa88ce65572629a891dc608c0d779a4
26841 <michael26841@gmail.com>
2019-11-14 01:12:06 -0800

    -

--- commands/art2.js
+++ commands/art2.js
@@ -10,7 +10,7 @@ module.exports = {
 	description: 'Get two random images from danbooru (sfw by danbooru ratings)',
 	async execute(message, args) {
 		try {
-			Booru.search(site, args, { limit: 10, random: true })
+			Booru.search(site, args.slice(0, args.length - 1), { limit: 100, random: true })
 				.then(posts => {
 					console.log(posts);
 					posts.filter(post => (post || {}).rating === 's' && (post || {}).previewUrl !== null).take(2),
