5495d0d0360048b6ab88b61a1b92701fef3c34b6
26841 <michael26841@gmail.com>
2019-11-13 20:02:47 -0800

    nsfw test

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -16,7 +16,7 @@ module.exports = {
 			try {
 				await each(
 					_(await search(site, args, { limit: 100, random: true }))
-						.filter(post => (post || {}).rating !== 's')
+						.filter(post => {(post || {}).rating !== 's' && (post || {}).previewUrl !== null;})
 						.take(2),
 					post => {console.log(post); message.channel.send(post.postView);},
 				);
