19af9500e78795a8f0ac4e6e94f06dce9ba6afa6
26841 <52977656+26841@users.noreply.github.com>
2019-11-13 22:55:33 -0800

    Update art.js

--- commands/art.js
+++ commands/art.js
@@ -4,6 +4,7 @@ const { search } = require('booru');
 const site = 'danbooru';
 // for ES6:
 // import Booru, { search, BooruError, sites } from 'booru'
+const tags = ['nagato_(kantai_collection)', 'mutsu_(kantai_collection)'];
 
 module.exports = {
 	name: 'art',
@@ -11,14 +12,14 @@ module.exports = {
 	async execute(message, args) {
 		try {
 			await each(
-				_(await search(site, ['nagato_(kantai_collection)', 'mutsu_(kantai_collection)'], { limit: 100, random: true }))
+				_(await search(site, tags.slice(0, tags.length - 1), { limit: 100, random: true }))
 					.filter(post => (post || {}).rating === 's' && (post || {}).previewUrl !== null)
 					.take(2),
-				post => {console.log(args); message.channel.send(post.postView);},
+				post => {console.log(tags); message.channel.send(post.postView);},
 			);
 		}
 		catch (error) {
-			if (args.length > 2) {
+			if (tags.length > 2) {
 				message.reply('You cannot search for more than two tags at a time!');
 			}
 			else {
