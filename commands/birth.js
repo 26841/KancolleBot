580d3d6920794892bd5f5bdb2c6b30fb3c34ffed
26841 <michael26841@gmail.com>
2019-11-15 02:50:38 -0800

    improved birth command

--- commands/birth.js
+++ commands/birth.js
@@ -2,6 +2,9 @@ const Discord = require('discord.js');
 const d = new Date();
 const birthdays = require('../birthday2.json');
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+function ordinal(date) {
+	return (date > 20 || date < 10) ? ([false, 'st', 'nd', 'rd'])[(date % 10)] || 'th' : 'th';
+}
 
 module.exports = {
 	name: 'birth',
@@ -16,7 +19,10 @@ module.exports = {
 			console.log(name);
 			for (let i = 0; i < birthdays.length; i++) {
 				if (birthdays[i].Name.toUpperCase() === name.toUpperCase()) {
-					return message.channel.send(birthdays[i].Month + ' ' + birthdays[i].Day + ' ' + birthdays[i].Year);
+					return message.channel.send(
+						new Discord.RichEmbed()
+							.setTitle(birthdays[i].Name + '\'s birthday is on ' + months[birthdays[i].Month - 1] + ' ' + ordinal(birthdays[i].Day) + ', ' + birthdays[i].Year),
+					);
 				}
 			}
 			return message.reply('Cannot find name.');
