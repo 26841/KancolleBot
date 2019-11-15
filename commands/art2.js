26b3f9aa04399b1eb0612f0bba7a1866c1468814
26841 <michael26841@gmail.com>
2019-11-15 00:22:11 -0800

    -

--- commands/art2.js
+++ commands/art2.js
@@ -1,6 +1,5 @@
-const { each } = require('bluebird');
-const _ = require('lodash');
 const Booru = require('booru');
+const { BooruError, sites } = require('booru');
 const site = 'danbooru';
 // for ES6:
 // import Booru, { search, BooruError, sites } from 'booru'
@@ -8,24 +7,27 @@ const site = 'danbooru';
 module.exports = {
 	name: 'art2',
 	description: 'Get two random images from danbooru (sfw by danbooru ratings)',
-	async execute(message, args) {
-		try {
-			await each(
-				_(await Booru.search(site, args, { limit: 100, random: true }))
-					.filter(post => (post || {}).rating === 's' && (post || {}).previewUrl !== null)
-					.take(2),
-				post => {console.log(args); message.channel.send(post.postView);},
-			);
-		}
-		catch (error) {
-			console.log(args);
-			if (args.length > 2) {
-				message.reply('You cannot search for more than two tags at a time!');
-			}
-			else {
-				message.channel.send('Something went wrong.');
-				console.error(error.message || error);
-			}
-		}
+	execute(message, args) {
+		let count = 0;
+		Booru.search(site, args, { limit: 100, random: true })
+			.then(posts => {
+				for (const post of posts) {
+					if (count === 2) {
+						break;
+					}
+					if (post.rating === 's') {
+						count++;
+						message.channel.send(post.postView);
+					}
+				}
+			})
+			.catch(err => {
+				if (err instanceof BooruError) {
+					console.error(err.message);
+				}
+				else {
+					console.error(err);
+				}
+			});
 	},
 };
